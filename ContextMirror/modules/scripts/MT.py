6cc28e52ccb9281baf4e8e1c09043edf67e6743a
m-iguel <73526976+m-iguel@users.noreply.github.com>
2025-01-14 17:20:31 +0100

    Update MT.py

--- ContextMirror/modules/scripts/MT.py
+++ ContextMirror/modules/scripts/MT.py
@@ -9,9 +9,9 @@ currentdir=os.path.dirname('../')
 
 def tree_tree_matrix(my_runs_file, commonsp_threshold,my_runs_pairs):
     subprocess.call('cat %s | parallel'%(my_runs_file),shell=True)
-    subprocess.call('mv %s my_runs/'%(my_runs_file),shell=True)
     subprocess.call('bash %s/ContextMirror/modules/scripts/MTscripts/ContextMirror-2.sh %s'%(currentdir,commonsp_threshold),shell=True)
     #os.system('bash %s'%(my_runs_pairs))
     subprocess.call('cat %s | parallel'%(my_runs_pairs),shell=True)
     #subprocess.call(f'cat {my_runs_pairs} | parallel --jobs 16', shell=True)
     subprocess.call('mv %s my_runs/'%(my_runs_pairs),shell=True)
+    subprocess.call('mv %s my_runs/'%(my_runs_file),shell=True)
